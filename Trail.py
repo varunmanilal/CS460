74a65fa70cafd242e27796a7f781d8f921f62d08
Varun <42139581+varunmanilal@users.noreply.github.com>
2021-09-23 19:04:25 +0530

    Delete Trail.py

--- Trail.py
+++
@@ -1,8 +0,0 @@
-from numpy import random
-from matplotlib import pyplot
-x = random.rand(100)
-c = random.rand(100)
-pyplot.plot(x,2*x+c,'s')
-pyplot.xlabel("X Value")
-pyplot.ylabel("Y Value")
-pyplot.show()
